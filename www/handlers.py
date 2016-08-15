90218e27d60363c308d3845106509d5782bf7926
Yuting Pang <4yutingpang@gmail.com>
2016-08-15 16:55:01 -0700

    fixed bug

---
+++ www/handlers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pyhton3
+# -*- coding: utf-8 -*-
+
+'''
+url handlers
+'''
+
+import re
+import time
+import json
+import logging
+import hashlib
+import base64
+import asyncio
+from coroweb import get, post
+from models import User, Comment, Blog, next_id
+
+@get('/')
+async def index(request):
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
